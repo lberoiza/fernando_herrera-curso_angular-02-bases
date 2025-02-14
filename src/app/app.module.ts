13ac272174e1458786624cfaae06f748f64d6e88
Luis B <luis.beroiza@hotmail.com>
2025-02-14 17:25:12 +0100

    agrega componentes hero y hero-list al app.module.ts
    tambien al app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './componets/counter/counter.component';
+import { HeroComponent } from './modules/heroes/hero/hero.component';
+import { HeroListComponent } from './modules/heroes/hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
