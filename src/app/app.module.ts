e36f4ff3b0af2e288f009e4d6998532c5e6763a8
Luis B <luis.beroiza@hotmail.com>
2025-02-14 17:29:11 +0100

    move counter component to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './componets/counter/counter.component';
+import { CounterComponent } from './modules/shared/components/counter/counter.component';
 import { HeroComponent } from './modules/heroes/hero/hero.component';
 import { HeroListComponent } from './modules/heroes/hero-list/hero-list.component';
 
