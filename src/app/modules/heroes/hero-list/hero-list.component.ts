d070973a5492845f3a9abbc520ad220371289c45
Luis B <luis.beroiza@hotmail.com>
2025-02-14 17:23:29 +0100

    agrega modulo heroes
    con 2 componentes hero y hero-list

---
+++ src/app/modules/heroes/hero-list/hero-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-list',
+  standalone: false,
+  templateUrl: './hero-list.component.html',
+  styleUrl: './hero-list.component.css'
+})
+export class HeroListComponent {
+
+}
