56d890fdac88feb7960426d5d8e5ddc32c1de281
Luis B <luis.beroiza@hotmail.com>
2025-02-14 17:30:07 +0100

    rename custom tags of module heros

--- src/app/modules/heroes/hero-list/hero-list.component.ts
+++ src/app/modules/heroes/hero-list/hero-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero-list',
+  selector: 'app-heros-hero-list',
   standalone: false,
   templateUrl: './hero-list.component.html',
   styleUrl: './hero-list.component.css'
--- src/app/modules/heroes/hero/hero.component.ts
+++ src/app/modules/heroes/hero/hero.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'app-heros-hero',
   standalone: false,
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
