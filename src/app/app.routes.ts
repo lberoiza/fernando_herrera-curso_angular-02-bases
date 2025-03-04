b41d3a7070f1e8e851cc5d351c9f16fd0d9a3437
Luis B <luis.beroiza@hotmail.com>
2025-03-04 21:02:22 +0100

    add hero component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-export const routes: Routes = [{
-  path: '',
-  component: CounterPageComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CounterPageComponent
+  },
+
+  {
+    path: 'hero',
+    component: HeroPageComponent
+  },
+];
---
+++ src/app/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,55 @@
+import { Component, signal } from '@angular/core';
+import { Hero } from '../../models/Hero.model';
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-page',
+  imports: [
+    UpperCasePipe,
+    TitleCasePipe
+  ],
+  templateUrl: './hero-page.component.html',
+  styleUrl: './hero-page.component.css'
+})
+export class HeroPageComponent {
+
+  readonly ironMan: Hero = {
+    name: 'IronMan',
+    age: 45
+  }
+
+  readonly spiderman: Hero = {
+    name: 'Spiderman',
+    age: 22
+  }
+
+  hero = signal<Hero>(this.copyHero(this.ironMan));
+
+  getHeroDescription() {
+    return `${this.hero().name} is ${this.hero().age} years old`;
+  }
+
+  changeHero() {
+    this.hero.set(this.copyHero(this.spiderman));
+  }
+
+  resetForm() {
+    this.hero.set(this.copyHero(this.ironMan));
+  }
+
+  changeAge() {
+    this.hero.update(currentHero => {
+      currentHero.age = 60;
+      return currentHero;
+    })
+  }
+
+  private copyHero(source: Hero): Hero {
+    return {
+      name: source.name,
+      age: source.age
+    }
+  }
+
+
+}
