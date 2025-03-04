a44500cbf56c5d2b1d88b56193b5545ba766e2ce
Luis B <luis.beroiza@hotmail.com>
2025-03-04 21:57:13 +0100

    add button bar component

---
+++ src/app/components/button-bar/button-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-button-bar',
+  imports: [],
+  templateUrl: './button-bar.component.html',
+  styleUrl: './button-bar.component.css'
+})
+export class ButtonBarComponent {
+
+}
