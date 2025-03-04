64f2f3dd6004335a6f4693878bce6fb02c1d35af
Luis B <luis.beroiza@hotmail.com>
2025-03-04 14:13:48 +0100

    using Signals to controle variables

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -9,11 +9,16 @@ import { Component } from '@angular/core';
 export class CounterPageComponent {
   protected counter: number = 0;
 
+  //using Signals
+  protected counterSignal: WritableSignal<number> = signal(0);
+
   protected increaseBy(number: number) {
     this.counter += number;
+    this.counterSignal.update((currentValue: number) => currentValue + number);
   }
 
   protected resetCounter() {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
