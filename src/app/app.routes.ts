6d1c5ec07fe5d3397924ac26f7249feaf59bf9d2
Luis B <luis.beroiza@hotmail.com>
2025-03-05 14:28:44 +0100

    add Bootstrap again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball-page/dragonball-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: CounterPageComponent
   },
-
   {
     path: 'hero',
     component: HeroPageComponent
+  },
+  {
+    path: 'dragonball',
+    component: DragonballPageComponent
   },
   {
     path: '**',
