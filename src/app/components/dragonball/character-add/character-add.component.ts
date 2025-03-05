b16e5ac9b12920cd1a18e83452bec8f54892d304
Luis B <luis.beroiza@hotmail.com>
2025-03-05 22:21:34 +0100

    create character add component Using Output Signal
    extract logic from add character to an own list component and use output signal to return the new character

---
+++ src/app/components/dragonball/character-add/character-add.component.ts
@@ -0,0 +1,40 @@
+import { Component, computed, output, signal } from '@angular/core';
+import type { DragonballCharacter } from '../../../models/DragonballCharacter';
+
+@Component({
+  selector: 'app-dragonball-character-add',
+  imports: [],
+  templateUrl: './character-add.component.html',
+  styleUrl: './character-add.component.css'
+})
+export class CharacterAddComponent {
+
+  newCharacterEvent = output<DragonballCharacter>()
+
+  newCharacterName = signal<string>('');
+  newCharacterPower = signal<number>(0);
+
+  newCharacterPreview = computed(() => `${this.newCharacterName()} (${this.newCharacterPower()})`);
+
+  addCharacter() {
+    if (!this.newCharacterName().trim() || this.newCharacterPower() <= 0) {
+      console.error('No name or Not enough Power!');
+      return;
+    }
+
+    const newCharacter: DragonballCharacter = {
+      id: Math.random() + 1000,
+      name: this.newCharacterName().trim(),
+      power: this.newCharacterPower()
+    }
+
+    this.newCharacterEvent.emit(newCharacter);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.newCharacterName.set('');
+    this.newCharacterPower.set(0);
+  }
+
+}
--- src/app/pages/dragonball-super-page/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super-page/dragonball-super-page.component.ts
@@ -1,12 +1,14 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import type { DragonballCharacter } from '../../models/DragonballCharacter';
 import { DragonballCharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 @Component({
   selector: 'app-dragonball-super-page',
   templateUrl: './dragonball-super-page.component.html',
   imports: [
-    DragonballCharacterListComponent
+    DragonballCharacterListComponent,
+    CharacterAddComponent
   ],
   styleUrl: './dragonball-super-page.component.css'
 })
@@ -17,29 +19,8 @@ export class DragonballSuperPageComponent {
     {id: 3, name: 'Vegeta', power: 8000},
   ])
 
-  newCharacterName = signal<string>('');
-  newCharacterPower = signal<number>(0);
 
-
-  newCharacterPreview = computed(() => `${this.newCharacterName()} (${this.newCharacterPower()})`);
-
-
-  addCharacter() {
-    if (!this.newCharacterName().trim() || this.newCharacterPower() <= 0) {
-      console.error('No name or Not enough Power!');
-      return;
-    }
-    const newCharacter: DragonballCharacter = {
-      id: this.characters().reduce((max, character) => character.id > max ? character.id : max, 0) + 1,
-      name: this.newCharacterName().trim(),
-      power: this.newCharacterPower()
-    }
+  saveCharacter(newCharacter: DragonballCharacter) {
     this.characters.update(currentList => [...currentList, newCharacter]);
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.newCharacterName.set('');
-    this.newCharacterPower.set(0);
   }
 }
