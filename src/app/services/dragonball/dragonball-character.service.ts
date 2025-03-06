cf9feed97d40b86b265754f3fdb7162ac2add9bd
Luis B <luis.beroiza@hotmail.com>
2025-03-06 14:05:13 +0100

    save and load dragonball characters to LocalStorage

--- src/app/services/dragonball/dragonball-character.service.ts
+++ src/app/services/dragonball/dragonball-character.service.ts
@@ -1,18 +1,30 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { effect, Injectable, signal, WritableSignal } from '@angular/core';
 import type { DragonballCharacter } from '../../models/DragonballCharacter';
+
+const CHARACTER_KEY = 'characters';
+
+// esta funcion flecha debe estar disponible antes de que la clase del servicio se cargue
+// por lo que no puede estar dentro de la clase
+const loadFromLocalStorage = (): DragonballCharacter[] => {
+  const localStorageInfo = localStorage.getItem(CHARACTER_KEY);
+  console.log(localStorageInfo);
+  return localStorageInfo ? JSON.parse(localStorageInfo) : [];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragonballCharacterService {
 
-  characters: WritableSignal<DragonballCharacter[]> = signal<DragonballCharacter[]>([
-    {id: 1, name: 'Goku', power: 9000},
-    {id: 3, name: 'Vegeta', power: 8000},
-  ])
-
+  characters: WritableSignal<DragonballCharacter[]> = signal<DragonballCharacter[]>(loadFromLocalStorage())
 
   saveCharacter(newCharacter: DragonballCharacter) {
     this.characters.update(currentList => [...currentList, newCharacter]);
   }
+
+  // if the characterSignal changes, this method will be called automatically
+  private saveToLocalStorage = effect(() => {
+    localStorage.setItem(CHARACTER_KEY, JSON.stringify(this.characters()));
+  });
+
 }
